ad28ea5ac36630c212f42e61b2562276307276a8
Danny Nguyen <danny3698@gmail.com>
2023-05-23 21:39:17 -0700

    Added resizable window

--- reactmessagingv1.fe/src/components/ChatComponent.js
+++ reactmessagingv1.fe/src/components/ChatComponent.js
@@ -1,9 +1,33 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { useAuth } from '../helpers/AuthContext';
 import PerfectScrollbar from 'react-perfect-scrollbar';
 import 'react-perfect-scrollbar/dist/css/styles.css';
 import ResizableWindowComponent from './ResizableWindowComponent';
 import './ChatComponent.css';
+
+const formatDateTime = (dateTimeString) => {
+    const dateTime = new Date(dateTimeString);
+    const now = new Date();
+
+    if (dateTime.toDateString() === now.toDateString()) {
+        return `Today, ${dateTime.toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric' })}`;
+    }
+
+    const yesterday = new Date(now);
+    yesterday.setDate(yesterday.getDate() - 1);
+    if (dateTime.toDateString() === yesterday.toDateString()) {
+        return `Yesterday, ${dateTime.toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric' })}`;
+    }
+
+    const options = {
+        month: 'short',
+        day: 'numeric',
+        year: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+    };
+    return dateTime.toLocaleDateString('en-US', options);
+};
 
 const ChatComponent = ({ session, connection, messages }) => {
     const [messageInput, setMessageInput] = useState('');
@@ -14,45 +38,17 @@ const ChatComponent = ({ session, connection, messages }) => {
     const [isMaximized, setIsMaximized] = useState(false);
 
     useEffect(() => {
-        const formattedMessages = messages.map((message) => ({
-            user: message.senderUserName,
-            text: message.text,
-            time: message.sentAt,
-        }));
-        setMessageList(formattedMessages);
-    }, [messages]);
+        console.log(messageList);
+    }, [messageList]);
 
-    useEffect(() => {
-        if (connection) {
-            connection.on(
-                'ReceiveMessage',
-                (sessionId, senderUserId, message, senderUserName, currentTime) => {
-                    if (sessionId === session?.id) {
-                        console.log(message);
-                        const newMessage = {
-                            user: senderUserName,
-                            text: message,
-                            time: currentTime,
-                        };
-                        setMessageList((prevMessages) => [...prevMessages, newMessage]);
-                    }
-                }
-            );
-
-            // clean up on unmount
-            return () => {
-                connection.off('ReceiveMessage');
-            };
-        }
-    }, [connection, session?.id]);
-
-    const handleSendMessage = async () => {
+    const handleSendMessage = useCallback(async () => {
         if (messageInput.trim() !== '' && connection) {
             await connection
                 .invoke('SendMessageToUser', session?.user?.id, currentUser?.email, messageInput)
                 .catch((err) => console.log(err));
 
             const newMessage = {
+                userId: currentUser?.userId,
                 user: currentUser?.email,
                 text: messageInput,
                 time: new Date().toISOString(),
@@ -61,14 +57,43 @@ const ChatComponent = ({ session, connection, messages }) => {
             setMessageList((prevMessages) => [...prevMessages, newMessage]);
             setMessageInput('');
         }
-    };
+    }, [connection, currentUser, messageInput, session]);
 
-    const handleKeyDown = (e) => {
+    const handleKeyDown = useCallback((e) => {
         if (e.key === 'Enter') {
             e.preventDefault();
             handleSendMessage();
         }
-    };
+    }, [handleSendMessage]);
+
+    useEffect(() => {
+        if (!connection) return;
+        const onReceiveMessage = (sessionId, senderUserId, message, senderUserName, currentTime) => {
+            if (sessionId !== session?.id) return;
+            setMessageList((prevMessages) => [
+                ...prevMessages,
+                {
+                    userId: senderUserId,
+                    user: senderUserName,
+                    text: message,
+                    time: currentTime,
+                },
+            ]);
+        };
+
+        connection.on('ReceiveMessage', onReceiveMessage);
+        return () => connection.off('ReceiveMessage', onReceiveMessage);
+    }, [connection, session?.id]);
+
+    useEffect(() => {
+        const formattedMessages = messages.map((message) => ({
+            userId: message.senderId,
+            user: message.senderUserName,
+            text: message.text,
+            time: message.sentAt,
+        }));
+        setMessageList(formattedMessages);
+    }, [messages]);
 
     const handleMinimize = () => {
         setIsMinimized(true);
@@ -83,26 +108,14 @@ const ChatComponent = ({ session, connection, messages }) => {
         console.log('Chat window closed');
     };
 
-    const formatDateTime = (dateTimeString) => {
-        const dateTime = new Date(dateTimeString);
-        const options = {
-            month: 'short',
-            day: 'numeric',
-            year: 'numeric',
-            hour: 'numeric',
-            minute: 'numeric',
-        };
-        return dateTime.toLocaleDateString('en-US', options);
-    };
-
-    if (!currentUser?.email || isMinimized) {
+    if (!currentUser?.email) {
         return null;
     }
 
     return (
         <ResizableWindowComponent
-            initialPosition={{ top: '2%', left: '25%' }}
-            initialSize={{ width: '900px', height: '930px' }}
+            initialPosition={{ top: 10, left: 10 }}
+            initialSize={{ width: window.innerWidth / 2, height: window.innerHeight / 2 }}
             renderHeader={() => (
                 <div className="chat-header">
                     <div className="window-title">
@@ -118,11 +131,12 @@ const ChatComponent = ({ session, connection, messages }) => {
         >
             <PerfectScrollbar options={{ wheelPropagation: true }} className="flex-grow-1">
                 {messageList.map((message, index) => (
-                    <div key={index} className="message-container d-flex align-items-start mb-3">
+                    <div className={`message-container d-flex align-items-start mb-3 px-2`}>
                         <div className="message-avatar bg-primary d-flex align-items-center justify-content-center rounded-circle">
                             {message.user?.charAt(0)}
                         </div>
-                        <div className="message-content">
+                        <div key={`${message.userId}-${index}`}
+                            className={`message-content w-100 ${message.userId === currentUser.userId ? 'bg-secondary' : 'bg-light'}`}>
                             <div className="message-sender">{message.user}</div>
                             <div className="message-text">{message.text}</div>
                             <div className="message-time">{formatDateTime(message.time)}</div>
@@ -130,7 +144,7 @@ const ChatComponent = ({ session, connection, messages }) => {
                     </div>
                 ))}
             </PerfectScrollbar>
-            <div className="chat-input d-flex align-items-center mt-2 mb-1">
+            <div className="chat-input d-flex align-items-center">
                 <input
                     type="text"
                     className="form-control rounded-0 shadow-none"
@@ -139,7 +153,12 @@ const ChatComponent = ({ session, connection, messages }) => {
                     onChange={(e) => setMessageInput(e.target.value)}
                     onKeyDown={handleKeyDown}
                 />
-                <button className="btn btn-primary ml-auto rounded-pill" onClick={handleSendMessage}>
+                <button
+                    className="btn btn-primary ml-auto rounded-pill"
+                    onClick={handleSendMessage}
+                    disabled={messageInput.trim() === ''}
+                    style={{ cursor: messageInput.trim() === '' ? 'default' : 'pointer' }}
+                >
                     Send
                 </button>
             </div>
--- reactmessagingv1.fe/src/components/ResizableWindowComponent.js
+++ reactmessagingv1.fe/src/components/ResizableWindowComponent.js
@@ -1,121 +1,39 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import { Rnd } from 'react-rnd';
 import './ResizableWindowComponent.css';
 
 const ResizableWindowComponent = ({ initialPosition, initialSize, renderHeader, children }) => {
-    const [windowPosition, setWindowPosition] = useState(initialPosition);
-    const [windowSize, setWindowSize] = useState(initialSize);
-    const [isDragging, setIsDragging] = useState(false);
-    const [isResizing, setIsResizing] = useState(false);
-    const [dragOffset, setDragOffset] = useState({ x: 0, y: 0 });
-    const [resizeDirection, setResizeDirection] = useState(null);
+    const [position, setPosition] = useState(initialPosition);
+    const [size, setSize] = useState(initialSize);
 
-    const handleHeaderMouseDown = (e) => {
-        e.preventDefault();
-        const offsetX = e.clientX - e.currentTarget.getBoundingClientRect().left;
-        const offsetY = e.clientY - e.currentTarget.getBoundingClientRect().top;
+    const onDragStop = (e, data) => {
+        setPosition({ top: data.y, left: data.x });
+    }
 
-        setIsDragging(true);
-        setIsResizing(false);
-        setDragOffset({ x: offsetX, y: offsetY });
-    };
-
-    const handleBorderMouseDown = (e) => {
-        e.preventDefault();
-        const offsetX = e.clientX - e.currentTarget.getBoundingClientRect().left;
-        const offsetY = e.clientY - e.currentTarget.getBoundingClientRect().top;
-
-        const edgeSize = 10; // size of the edge area where the user can start resizing
-        const { width, height } = e.currentTarget.getBoundingClientRect();
-        const isOnTopEdge = offsetY < edgeSize;
-        const isOnBottomEdge = offsetY > height - edgeSize;
-        const isOnLeftEdge = offsetX < edgeSize;
-        const isOnRightEdge = offsetX > width - edgeSize;
-
-        if (isOnTopEdge || isOnBottomEdge || isOnLeftEdge || isOnRightEdge) {
-            setIsResizing(true);
-            setIsDragging(false);
-            setResizeDirection({
-                horizontal: isOnRightEdge ? 'right' : isOnLeftEdge ? 'left' : null,
-                vertical: isOnBottomEdge ? 'bottom' : isOnTopEdge ? 'top' : null,
-            });
-        }
-    };
-
-    const handleMouseMove = (e) => {
-        e.preventDefault();
-
-        if (isDragging) {
-            const newX = e.clientX - dragOffset.x;
-            const newY = e.clientY - dragOffset.y;
-            setWindowPosition({ top: `${newY}px`, left: `${newX}px` });
-        } else if (isResizing) {
-            const dx = e.clientX - dragOffset.x;
-            const dy = e.clientY - dragOffset.y;
-            console.log('resizeDirection:', resizeDirection);
-            if (resizeDirection === 'left') {
-                setWindowPosition((prevPosition) => ({ ...prevPosition, left: `${prevPosition.left + dx}px` }));
-                setWindowSize((prevSize) => ({ ...prevSize, width: `${prevSize.width - dx}px` }));
-            } else if (resizeDirection === 'right') {
-                setWindowSize((prevSize) => ({ ...prevSize, width: `${prevSize.width + dx}px` }));
-            } else if (resizeDirection === 'top') {
-                setWindowPosition((prevPosition) => ({ ...prevPosition, top: `${prevPosition.top + dy}px` }));
-                setWindowSize((prevSize) => ({ ...prevSize, height: `${prevSize.height - dy}px` }));
-            } else if (resizeDirection === 'bottom') {
-                setWindowSize((prevSize) => ({ ...prevSize, height: `${prevSize.height + dy}px` }));
-            } else if (resizeDirection === 'top-left') {
-                setWindowPosition((prevPosition) => ({ ...prevPosition, top: `${prevPosition.top + dy}px`, left: `${prevPosition.left + dx}px` }));
-                setWindowSize((prevSize) => ({ ...prevSize, width: `${prevSize.width - dx}px`, height: `${prevSize.height - dy}px` }));
-            } else if (resizeDirection === 'top-right') {
-                setWindowPosition((prevPosition) => ({ ...prevPosition, top: `${prevPosition.top + dy}px` }));
-                setWindowSize((prevSize) => ({ ...prevSize, width: `${prevSize.width + dx}px`, height: `${prevSize.height - dy}px` }));
-            } else if (resizeDirection === 'bottom-left') {
-                setWindowPosition((prevPosition) => ({ ...prevPosition, left: `${prevPosition.left + dx}px` }));
-                setWindowSize((prevSize) => ({ ...prevSize, width: `${prevSize.width - dx}px`, height: `${prevSize.height + dy}px` }));
-            } else if (resizeDirection === 'bottom-right') {
-                setWindowSize((prevSize) => ({ ...prevSize, width: `${prevSize.width + dx}px`, height: `${prevSize.height + dy}px` }));
-            }
-        }
-    };
-
-
-    const handleMouseUp = (e) => {
-        e.preventDefault();
-        setIsDragging(false);
-        setIsResizing(false);
-    };
-
-    useEffect(() => {
-        if (isDragging || isResizing) {
-            window.addEventListener('mousemove', handleMouseMove);
-            window.addEventListener('mouseup', handleMouseUp);
-        } else {
-            window.removeEventListener('mousemove', handleMouseMove);
-            window.removeEventListener('mouseup', handleMouseUp);
-        }
-
-        return () => {
-            window.removeEventListener('mousemove', handleMouseMove);
-            window.removeEventListener('mouseup', handleMouseUp);
-        };
-    }, [isDragging, isResizing]);
+    const onResizeStop = (e, direction, ref, delta, position) => {
+        setSize({
+            width: ref.style.width,
+            height: ref.style.height,
+        });
+        setPosition({ top: position.y, left: position.x });
+    }
 
     return (
-        <div
-            className={`resizable-window${isDragging ? ' draggable' : ''}${isResizing ? ' resizing' : ''}`}
-            style={{
-                top: windowPosition.top,
-                left: windowPosition.left,
-                width: windowSize.width,
-                height: windowSize.height,
-                cursor: isDragging ? 'grabbing' : 'default',
-            }}
-            onMouseDown={handleBorderMouseDown}
+        <Rnd
+            size={{ width: size.width, height: size.height }}
+            position={{ x: position.left, y: position.top }}
+            onDragStop={onDragStop}
+            onResizeStop={onResizeStop}
+            dragHandleClassName="window-header"
+            bounds="window"
+            minWidth='30%'
+            minHeight='35%'
         >
-            <div className="window-header" onMouseDown={handleHeaderMouseDown}>
+            <div className="window-header">
                 {renderHeader()}
             </div>
             <div className="window-content">{children}</div>
-        </div>
+        </Rnd>
     );
 };
 
